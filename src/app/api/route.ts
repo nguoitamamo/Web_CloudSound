4bfe8c2e3338054260d05ad724fbe3d4edc50f5b
nguoitammao <huynhngoctruonghnt@gmail.com>
2025-08-01 14:07:16 +0700

    add src/app/api

---
+++ src/app/api/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, response: NextResponse) {
+    const url = new URL(request.url);
+    const searchParams = new URLSearchParams(url.search);
+    const audio = searchParams.get('audio');
+    return await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_ASSET}/audio/${audio}`);
+}
+
