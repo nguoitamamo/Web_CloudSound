32efd96ae365d05aaa74ba4ecf69c12081102a6f
Huỳnh Ngọc Trương <126697153+HuynhNgocTruongg@users.noreply.github.com>
2026-04-10 23:58:36 +0700

    Update page.tsx

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,47 +3,26 @@ import MainSlider from "@/components/main/main.slider";
 import { sendRequest } from "@/utils/api";
 import { Box, Container, Divider, Paper } from "@mui/material";
 import Vip from "@/components/vip/app.vip";
-
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import HomeHistory from "@/components/history/home.history";
 import MeetingTypeList from "@/components/MeetingTypeList";
-
-
-
-
-
 
 const HomePage = async () => {
 
   const session = await getServerSession(authOptions);
   const type = 'week';
 
-
-
   const songTrending = await sendRequest<IBackendRes<ISong[]>>({
     url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/songs/top/${type}`,
     method: "GET",
   })
-
-
-
-
-  // _app.tsx hoặc trong component chính
-
-
-
-
-
   return (
     <Container >
-
       <Box sx={{
         display: 'flex',
         justifyContent: 'space-evenly',
       }}>
-
-
         <Box sx={{ flex: 7 }}>
           <MainSlider data={songTrending?.data ?? []} type={type} topic={"Trending"} />
         </Box>
